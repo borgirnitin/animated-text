a5d749eb588a3812c322dbe1c2ea0187bbd0535a
borgirnitin <140273213+borgirnitin@users.noreply.github.com>
2025-05-03 14:07:51 -0700

    Created index.js

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import { useEffect, useMemo, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function Home() {
+  const titles = useMemo(() => ["amazing", "new", "wonderful", "beautiful", "smart"], []);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % titles.length);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [titles.length]);
+
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      backgroundColor: "#0D0D0D",
+      color: "#fff",
+      fontSize: "2rem",
+      fontWeight: "bold",
+    }}>
+      <span>You are&nbsp;</span>
+      <AnimatePresence mode="wait">
+        <motion.span
+          key={titles[index]}
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.5 }}
+          style={{ display: "inline-block" }}
+        >
+          {titles[index]}
+        </motion.span>
+      </AnimatePresence>
+    </div>
+  );
+}
