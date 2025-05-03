6562a079baedfa947724f94c0fba610d4e2595ba
borgirnitin <140273213+borgirnitin@users.noreply.github.com>
2025-05-03 14:51:27 -0700

    Update index.js - fixed designer

--- pages/index.js
+++ pages/index.js
@@ -29,41 +29,66 @@ export default function AnimatedHero() {
           alignItems: "center",
           height: "100vh",
           backgroundColor: "#0D0D0D",
-          color: "#FFFFFF",
-          fontFamily: "'General Sans', sans-serif",
-          fontSize: "2.5rem",
-          fontWeight: 700,
-          letterSpacing: "0.15em",
-          textTransform: "uppercase",
         }}
       >
-    
-        
-        <AnimatePresence mode="wait">
-          <motion.span
-            key={titles[index]}
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -20 }}
-            transition={{
-              duration: 0.6,
-              type: "spring",
-              stiffness: 200,
-              damping: 18,
-            }}
+        <div
+          style={{
+            position: "relative",
+            fontFamily: "'General Sans', sans-serif",
+            fontSize: "2.5rem",
+            fontWeight: 700,
+            letterSpacing: "0.15em",
+            textTransform: "uppercase",
+          }}
+        >
+          {/* Fixed "Designer" at right end */}
+          <span
             style={{
-              display: "inline-block",
-              marginRight: "0.5rem",
-              background: "linear-gradient(90deg, #F8D442, #FF6E7F)",
-              WebkitBackgroundClip: "text",
-              WebkitTextFillColor: "transparent",
+              position: "relative",
+              zIndex: 1,
+              paddingLeft: "200px", // Adjust this to control the distance from the animated word
             }}
           >
-            {titles[index]}
-          </motion.span>
-        </AnimatePresence>
+            Designer
+          </span>
 
-        <span>Designer</span>
+          {/* Animated Word Positioned Absolutely to Left */}
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "200px", // Same as paddingLeft on "Designer"
+              overflow: "hidden",
+              textAlign: "right",
+              zIndex: 0,
+            }}
+          >
+            <AnimatePresence mode="wait">
+              <motion.span
+                key={titles[index]}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{
+                  duration: 0.6,
+                  type: "spring",
+                  stiffness: 200,
+                  damping: 18,
+                }}
+                style={{
+                  display: "inline-block",
+                  background: "linear-gradient(90deg, #F8D442, #FF6E7F)",
+                  WebkitBackgroundClip: "text",
+                  WebkitTextFillColor: "transparent",
+                  whiteSpace: "nowrap",
+                }}
+              >
+                {titles[index]}
+              </motion.span>
+            </AnimatePresence>
+          </div>
+        </div>
       </div>
     </>
   );
