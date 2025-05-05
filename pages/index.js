9785bf71a3316145b4d8da303e48937d7e80254e
borgirnitin <140273213+borgirnitin@users.noreply.github.com>
2025-05-04 19:19:04 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function AnimatedHero() {
 
         .pill-wrapper {
           position: relative;
-          width: 13ch;
+          width: 10ch;
           height: 3.5rem;
           padding: 0 1.25rem;
           border-radius: 9999px;
